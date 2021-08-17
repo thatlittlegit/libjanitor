c7a5cc9713a7997fada1b485849ea0c5cf36e44c
thatlittlegit <personal@thatlittlegit.tk>
2021-08-17 18:49:16 -0400

    Add janitor_context management functions

--- include/noincludes.h
+++ include/noincludes.h
@@ -8,6 +8,36 @@
 #endif
 #define __janitor__noincludes_h__
 
+typedef void (janitor_free_t) (void *);
+
+/**
+ * @struct janitor_context
+ *
+ * Represents the data and functions that will be freed by janitor_clean. It's
+ * automatically freed.
+ */
+struct janitor_context;
+
+/**
+ * Creates a new, automatically-resizing Janitor context, using the default
+ * allocator.
+ *
+ * @returns the new allocator, or NULL on allocation failure
+ */
+struct janitor_context *janitor_context_new (void);
+
+/**
+ * Adds a function to the Janitor context. The function will be called with the
+ * provided element.
+ */
+void janitor_context_add (struct janitor_context *, void *, janitor_free_t *);
+
+/**
+ * Get the last thing to free in the Janitor context, removing it from the
+ * context.
+ */
+void janitor_context_iter (struct janitor_context *, void **,
+                           janitor_free_t **);
 /**
  * Schedules the given context to be cleaned at the end of the function.
  */
---
+++ src/context.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2021 thatlittlegit <personal@thatlittlegit.tk>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+#define _DEFAULT_SOURCE
+
+#include "../include/janitor.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+struct janitor_item
+{
+  void *ptr;
+  janitor_free_t *free;
+};
+
+struct janitor_context
+{
+  unsigned count;
+  struct janitor_item *items;
+};
+
+struct janitor_context *
+janitor_context_new (void)
+{
+  struct janitor_context *ctx = malloc (sizeof (struct janitor_context));
+  if (ctx == NULL)
+    return NULL;
+
+  ctx->count = 0;
+  ctx->items = NULL;
+  return ctx;
+}
+
+void
+janitor_context_add (struct janitor_context *ctx, void *ptr,
+                     janitor_free_t *free)
+{
+  assert (ctx != NULL);
+  assert (free != NULL);
+
+  struct janitor_item *items = reallocarray (ctx->items, ctx->count + 1,
+                                             sizeof (struct janitor_item));
+
+  assert (items != NULL);
+
+  ctx->items[ctx->count].ptr = ptr;
+  ctx->items[ctx->count].free = free;
+  ctx->items = items;
+  ctx->count += 1;
+}
+
+void
+janitor_context_iter (struct janitor_context *ctx, void **ptr,
+                      janitor_free_t **free)
+{
+  assert (ctx != NULL);
+
+  struct janitor_item *item = &ctx->items[ctx->count - 1];
+
+  if (ptr != NULL)
+    *ptr = item->ptr;
+
+  if (free != NULL)
+    *free = item->free;
+
+  struct janitor_item *items = reallocarray (ctx->items, ctx->count - 1,
+                                             sizeof (struct janitor_item));
+
+  if (items != NULL)
+    ctx->items = items;
+
+  ctx->count -= 1;
+}
