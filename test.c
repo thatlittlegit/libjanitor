76ee907d470b6d3ab15d513f8824115448ea2d70
thatlittlegit <personal@thatlittlegit.tk>
2021-08-17 22:57:23 -0400

    Pass a meaningful context to janitor_clean in test.c
    This is useful for testing and as an example.

--- test.c
+++ test.c
@@ -2,14 +2,16 @@
  * Copyright 2021 thatlittlegit <personal@thatlittlegit.tk>
  * SPDX-License-Identifier: CC0-1.0
  */
+#include "include/janitor.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-void janitor_clean (void *);
 
 int
 main ()
 {
-  janitor_clean (NULL);
+  void *data = malloc(128);
+  struct janitor_context *ctx = janitor_context_new();
+  janitor_context_add(ctx, data, free);
+  janitor_clean (ctx);
   printf ("hello, world!\n");
 }
